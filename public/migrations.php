e989e76bb0d1a4607318b6649ae6101e16a8a15b
Sebastian Berm <6470150+sebastianberm@users.noreply.github.com>
2025-12-30 21:57:55 +0000

    Added missing include

--- public/migrations.php
+++ public/migrations.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../includes/db.php';
 require __DIR__ . '/../includes/auth.php';
+require __DIR__ . '/../includes/functions.php';
 require_login();
 
 $u = current_user();
